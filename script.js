9462ca539199d1f06426d9d2ea5e4c962b69e648
Artur Tribeck Ferreira Tomaz <arturtomaz1@gmail.com>
2024-09-04 20:53:35 -0300

    map das listas com json

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+//JSON das tarefas
+let tarefasJson = [
+	{ id: 1, texto: "Lavar a louça" },
+	{ id: 2, texto: "Lavar a roupa" },
+];
+
 function marcar() {}
+
+tarefasJson.map((item, index) => {
+	let tarefaItem = document
+		.querySelector(".models .tarefa-item")
+		.cloneNode(true);
+	tarefaItem.setAttribute("data-key", index);
+	tarefaItem.querySelector(".checkbox").setAttribute("value", index);
+	tarefaItem.querySelector(".tarefa-texto").innerHTML = item.texto;
+
+	document.querySelector(".lista-toda").append(tarefaItem);
+});
