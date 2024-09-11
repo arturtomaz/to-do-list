6bf9a756301943b74fe094b3338d8431d00d5320
Artur Tribeck Ferreira Tomaz <arturtomaz1@gmail.com>
2024-09-11 18:46:31 -0300

    Adicionar Tarefa
    checked ainda não funcionando

--- script.js
+++ script.js
@@ -1,18 +1,51 @@
 //JSON das tarefas
-let tarefasJson = [
-	{ id: 1, texto: "Lavar a louça" },
-	{ id: 2, texto: "Lavar a roupa" },
-];
+let tarefasJson = [];
 
-function marcar() {}
+index_geral = -1;
 
-tarefasJson.map((item, index) => {
-	let tarefaItem = document
-		.querySelector(".models .tarefa-item")
-		.cloneNode(true);
-	tarefaItem.setAttribute("data-key", index);
-	tarefaItem.querySelector(".checkbox").setAttribute("value", index);
-	tarefaItem.querySelector(".tarefa-texto").innerHTML = item.texto;
+function addTask(tarefa) {
+	if (document.querySelector(".add-input").value == "") {
+		alert("Digite uma tarefa antes de clicar em adicionar!");
+	} else {
+		index_geral = index_geral + 1;
+		tarefasJson.push({ id: index_geral, texto: String(tarefa) });
+		console.log(tarefasJson);
+		document.querySelector(".add-input").value = "";
+		atualizarTarefas();
+	}
+}
+function atualizarTarefas() {
+	if (tarefasJson.length == 0) {
+		document.querySelector(".lista-toda").innerHTML = `<p>Não há tarefas!</p`;
+	} else {
+		document.querySelector(".lista-toda").innerHTML = "";
+		tarefasJson.map((item, index) => {
+			let tarefaItem = document
+				.querySelector(".models .tarefa-item")
+				.cloneNode(true);
+			tarefaItem.setAttribute("data-key", index);
+			tarefaItem.setAttribute("value", index);
+			tarefaItem.querySelector(".tarefa-texto").innerHTML = item.texto;
 
-	document.querySelector(".lista-toda").append(tarefaItem);
-});
+			document.querySelector(".lista-toda").append(tarefaItem);
+		});
+	}
+}
+atualizarTarefas();
+
+valoresMarcados = [];
+function marcar(value) {
+	if (document.querySelector(`.tarefa-item[value='${value}']`).checked) {
+		valoresMarcados.append(value);
+	} else {
+		let index = valoresMarcados.indexOf(value);
+		valoresMarcados.splice(index, 1);
+	}
+	console.log(valoresMarcados);
+}
+
+function remove() {
+	for (let i of valoresMarcados) {
+		document.querySelector(`.tarefa-item[value=${i}]`).remove();
+	}
+}
